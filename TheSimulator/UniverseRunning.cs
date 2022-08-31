e364f651721fdf0dcf0d1fbe7c7dc955be4fa3e5
camcb <68913310+CamIsAPenguin@users.noreply.github.com>
2022-08-31 15:49:19 -0400

    Work

--- TheSimulator/UniverseRunning.cs
+++ TheSimulator/UniverseRunning.cs
@@ -4,12 +4,41 @@ namespace Universe
 {
     public class RunningUniverse
     {
-        public static void Years(string UName)
+        public static void Years(string UName, bool stop = false)
         {
-            Console.WriteLine("------------------------------------------------------");
-            Console.WriteLine($"Welcome to {UName}, to exit this universe and return to command selection enter \"q\"");
-            Console.WriteLine("To advance the Simulation enter the amount of megannums (1 million years) to pass");
-            Console.WriteLine("------------------------------------------------------");
+            if(stop == false)
+            {
+                Console.WriteLine("=====================================================================================");
+                Console.WriteLine($"Welcome to {UName}, to exit this universe and return to command selection enter \"q\", to save enter \"s\"");
+                Console.WriteLine("To advance the Simulation enter the amount of megannums (1 million years) to pass");
+                Console.WriteLine("====================================================================================");
+                Console.WriteLine("");
+                
+            }
+
+            
+            while(stop == false)
+            {
+                string? CMD = Console.ReadLine();
+                CMD.ToUpper();
+                Quit(CMD, UName);
+            }
+            
+        }
+        public static void Quit(string CurrentCMD, string uname)
+        {
+            switch (CurrentCMD)
+            {
+                case "Q":
+                    bool stop = true;
+                    Years(uname, stop);
+                    Start.Startup.Main();
+                    // add case s to save
+                break;
+            }
+        }
+        internal static void UniverseStats()
+        {
             
         }
     }
