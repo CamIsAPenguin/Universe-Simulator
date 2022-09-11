e0220497ef099a93f2b693fe78245813409645ae
camcb <68913310+CamIsAPenguin@users.noreply.github.com>
2022-09-10 22:22:05 -0400

    Made the Universe Better

--- TheSimulator/UniverseRunning.cs
+++ TheSimulator/UniverseRunning.cs
@@ -13,7 +13,7 @@ namespace Universe
                 Console.WriteLine("To advance the Simulation enter the amount of megannums (1 million years) to pass");
                 Console.WriteLine("====================================================================================");
                 Console.WriteLine("");
-                THEUNIVERSE.TheUniverse(YearsToPass, UName);
+                THEUNIVERSE.TheUniverse(UName);
                 
             
         }
@@ -24,7 +24,7 @@ namespace Universe
     internal class THEUNIVERSE
     {
         public static string numofspec, numofplanets, numofintellife, numoflife, numofcivs,numofyears = "0", Event1, Event2, Event3;
-        internal static void TheUniverse(string YearsPassing, string UniverseName)
+        internal static void TheUniverse(string UniverseName)
         {
             char[] Alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'x', 'x', 'y', 'z'};
             char[] nums = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
@@ -54,69 +54,49 @@ namespace Universe
             string Event3Location;
             Console.WriteLine("====================================================================================");
             Console.WriteLine("");
-
             string CMDLine = Convert.ToString(Console.GetCursorPosition());
             Console.Write("CMD: ");
             //Console.WriteLine("");
 
             while(true)
             {
-                bool onlynums;
                 var CMD = Console.ReadLine();
                 CMD.ToLower();
-                
-                while(true)
+                for(int i = 0; i < 25; i++)
                 {
-                    int i = 0;
-                    char currentletter = Alphabet[i];
-                    char currentnum = nums[i];
-                    if(CMD.Contains(currentletter))
+                    char CurrentLetter  = Alphabet[i];
+                    if(CMD.Contains(CurrentLetter))
                     {
-                        Console.WriteLine("ERROR, PLEASE ENTER A VALID COMMAND");
-                    }
-                    if(CMD == "s")
-                    {
-                        ForceSave();
-                    }
-                    if(CMD.Contains(currentnum))
-                    {
-                        onlynums = true;
-                        break;
+                        Console.Write("ERROR");
                     }
                     else
                     {
-                        break;
+                        if(CMD == "q")
+                        {
+                            Quit();
+                        }
+                        if(CMD == "s")
+                        {
+                            ForceSave();
+                        }
+                        else
+                        {
+                            AdvanceUniverse(CMD, NumOfYearsStats, CMDLine, UniverseName);
+                        }
                     }
-                    
-                }   
+                }
 
-                if(CMD == "q")
-                {
-                    Quit();
-                }
-                if(CMD == "s")
-                {
-                    ForceSave();
-                }
-                if(onlynums = true)
-                {
-                    AdvanceUniverse(CMD, NumOfYearsStats);
-                }
-                else
-                {
-                    Console.WriteLine("ERROR, PLEASE ENTER A VALID COMMAND");
-                }
             }
 
         }
 
         //advance the simulation
-        public static void AdvanceUniverse(string yearspassing, string yearspos)
+        public static void AdvanceUniverse(string yearspassing, string yearspos, string beforepos, string Uname)
         {
-            yearspassing = yearspassing + "000,000";
+            Convert.ToInt32(yearspassing);
+            yearspassing = yearspassing + 1000000; //fix this
             numofyears = yearspassing;
-            string beforePos = Convert.ToString(Console.GetCursorPosition());
-            ChangeCursorPos(yearspos,beforePos, $"YEAR: {numofyears}");
+            ChangeCursorPos(yearspos,beforepos, $"YEAR: {numofyears}", Uname);
         }
         //advance the simulation
 
@@ -128,7 +108,7 @@ namespace Universe
         {
             
         }
-        public static void ChangeCursorPos(string CursorPos, string beforePos, string WhatToWrite)
+        public static void ChangeCursorPos(string CursorPos, string beforePos, string WhatToWrite, string UName)
         {
             //beforePos = Convert.ToString(Console.GetCursorPosition());
             string[] splitPosBefore = beforePos.Split(',');
@@ -154,17 +134,9 @@ namespace Universe
             Console.SetCursorPosition(iPos2,iPos1);
             Console.Write(WhatToWrite);
             Console.SetCursorPosition(iPos2before,iPos1before);
-            Console.CursorTop--;
-            Console.CursorLeft++;
-            Console.CursorLeft++;
-            Console.CursorLeft++;
-            Console.CursorLeft++;
-            Console.CursorLeft++;
-            Console.Write("    ");
-            Console.CursorLeft--;
-            Console.CursorLeft--;
-            Console.CursorLeft--;
-            Console.CursorLeft--;
+            Console.Clear();
+            THEUNIVERSE.TheUniverse(UName);
+            
 
 
         }
